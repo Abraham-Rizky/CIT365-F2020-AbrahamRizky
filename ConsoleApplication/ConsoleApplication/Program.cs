1dcfda988a853fcebdff3c16a7aab92afe92cbd7
Abraham-Rizky <rizky@DESKTOP-5ORJKVE>
2020-09-15 07:25:22 -0700

    Complete submit

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -21,6 +21,22 @@ namespace ConsoleApplication
             DateTime xmas = new DateTime(date.Year, 12, 25);
             double totalDaysUntilXmas = Math.Round((xmas - date).TotalDays);
             Console.WriteLine($"It is only {totalDaysUntilXmas} until Christmas!");
+
+            //Section 2.1 from the book
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("Please enter the width:");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Please enter the height:");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
         }
     }
 }
