2c80d2aa93b05141e8c8009fff3b1e1591d428bf
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2020-12-11 20:17:53 -0800

    Revert "Base data"
    This reverts commit fdb640cb95b5d4afef5e0dc89635ea2f93454a38.

---
+++ ConsoleAppNew/ConsoleAppNew/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name and location assignment
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.Write("Where are you from? ");
+            string location = Console.ReadLine();
+            Console.WriteLine($"My name is {name}, I am from {location}.");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            //Current date assignment
+            DateTime date = DateTime.Now;
+            //DateTime date = new DateTime(2020, 12, 27);
+            int thisYear = date.Year;
+            int thisMonth = date.Month;
+            int today = date.Day;
+            Console.WriteLine($"\nThe current date is {thisMonth}-{today}-{thisYear}");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            //Number of days until Christmas assignment
+            DateTime xmas = new DateTime(date.Year, 12, 25);
+
+            if (date > xmas)
+            {
+                xmas = new DateTime(date.Year + 1, 12, 25);
+            }
+            double totalDaysUntilXmas = Math.Round((xmas - date).TotalDays);
+            Console.WriteLine($"\nIt is only {totalDaysUntilXmas} until Christmas!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            //Section 2.1 from the book assignment
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("\nPlease enter the width:");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Please enter the height:");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyScriptureJournal/MyScriptureJournal/Migrations/20201021150653_Initial2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScriptureJournal.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Scripture",
+                newName: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Scripture",
+                newName: "ReleaseDate");
+        }
+    }
+}
